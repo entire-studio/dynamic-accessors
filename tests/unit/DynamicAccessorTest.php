8c2233b44529be38e87870fffd8d3249c0d7b603
Gormack <dev@entire.studio>
2023-06-06 17:09:31 +0200

    Tests (#5)
    * Tests
    * Updated README with a badge

---
+++ tests/unit/DynamicAccessorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Gormack\DynamicAccessors;
+
+use Gormack\DynamicAccessors\{DynamicAccessors, Get, Set};
+use PHPUnit\Framework\TestCase;
+
+class DynamicAccessorTest extends TestCase
+{
+    public function testTrait(): void
+    {
+        $cut = new class
+        {
+            use DynamicAccessors;
+
+            #[Set, Get] // The same as #[Set('firstName), Get('firstName)]
+            protected string $firstName = 'John';
+
+            #[Set('setLastName'), Get('getLastName')] // rename accessors
+            private $lastName = 'Doe';
+        };
+
+        $this->assertEquals('John', $cut->firstName());
+        $cut->firstName('NOT John');
+        $this->assertEquals('NOT John', $cut->firstName());
+
+        $this->assertEquals('Doe', $cut->getLastName());
+        $cut->setLastName('NOT Doe');
+        $this->assertEquals('NOT Doe', $cut->getLastName());
+    }
+}
