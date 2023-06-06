62c703628866d7b61ff1392369b9abf6b5ac1d45
Tomasz Jóźwik <dev@entire.studio>
2023-06-06 14:52:39 +0200

    Added CI, tests, coding style check, readme, fixed reported issues closes #1, #2, #3

---
+++ tests/unit/AccessorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Gormack\DynamicAccessors;
+
+use Gormack\DynamicAccessors\Accessor;
+use PHPUnit\Framework\TestCase;
+
+class AccessorTest extends TestCase
+{
+    /**
+     * @testdox Simple getters test
+     */
+    public function testGetters(): void
+    {
+        $type = 'Get';
+        $name = 'getFirstName';
+        $propertyName = 'firstName';
+
+        $accessor = new Accessor($type, $name, $propertyName);
+
+        $this->assertInstanceOf(Accessor::class, $accessor);
+        $this->assertEquals($type, $accessor->getType());
+        $this->assertEquals($name, $accessor->getName());
+        $this->assertEquals($propertyName, $accessor->getPropertyName());
+    }
+}
